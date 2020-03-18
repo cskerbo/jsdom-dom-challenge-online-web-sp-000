2a4c16a9aa9bf96622a9c6491fcce8e14a4cd68b
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 01:19:21 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -5,11 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
 const counterField = document.getElementById('counter')
 const minusButton = document.querySelector('#minus')
 const plusButton = document.querySelector('#plus')
+const pauseButton = document.querySelector('#pause')
+const heartButton = document.querySelector('#heart')
+const likes = document.querySelector('.likes')
+const submitButton = document.querySelector('#submit')
 
 function counter() {
   let i = 0
   setInterval(function() {
-    counterField.innerHTML = i++
+    counterField.innerText++;
   }, 1000);
 
   function countUp(){
