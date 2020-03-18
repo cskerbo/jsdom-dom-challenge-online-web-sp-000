d94b3173b14ab633600be510ad9156398ffab3d7
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 01:05:21 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -4,6 +4,7 @@ let counterField = document.getElementById('counter')
 function counter() {
   let i = 0
   setInterval(function() {
+    counterField.innerHTML = i++
     console.log((i++));
   }, 1000);
 
