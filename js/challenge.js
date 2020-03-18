ecba133cab8268b8bd090e55635935482dcaec08
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 02:16:37 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -56,7 +56,7 @@ heartButton.addEventListener('click', function(e){
 submitButton.addEventListener('click', function(e){
   e.preventDefault()
   let userComment = document.getElementById('comment-input').value
-  commentList.innerHTML += `<p>${userComment}</p>`
+  commentList.innerHTML += `<p>User said "${userComment}"</p>`
 });
 
 });
