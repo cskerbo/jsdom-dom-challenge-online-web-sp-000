d33dcc22c96854487031ebcb79e34b88070c0d23
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 02:04:57 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -51,6 +51,6 @@ pauseButton.addEventListener('click', function(e) {
 likeButton.addEventListener('click', function(e){
   let likedNumber = counterField.innerText
   likes.innerHTML += `<li>${likedNumber} is liked 1 times.</li>`
-})
+});
 
 });
