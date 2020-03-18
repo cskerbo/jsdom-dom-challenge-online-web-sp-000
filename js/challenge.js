b86f8b0618269f5b675454ff99ed747664634cbe
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-18 01:36:50 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -29,7 +29,7 @@ plusButton.addEventListener('click', function(e) {
 pauseButton.addEventListener('click', function(e) {
   if (pauseButton.innerText === 'Pause'){
     pauseButton.innerText = "Resume"
-    clearInterval(counter)
+    clearInterval(counter())
 
 
     minusButton.disabled = true;
